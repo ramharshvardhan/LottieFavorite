dae0805b1030f81db1c00117f0af4a9f753b8e86
Ram Radhakrishnan <rradhakrishnan@wayfair.com>
2019-04-23 14:34:52 -0400

    Initial project setup for LottieFavorite.

---
+++ LottieFavorite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieFavorite
+//
+//  Created by Ram Radhakrishnan on 4/23/19.
+//  Copyright © 2019 Ram Radhakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
